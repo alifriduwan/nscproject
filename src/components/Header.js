f0c975640b781c6633185860b63b7fae039ab06e
psu6510110004 <dragonslayertitle@gmail.com>
2023-02-27 17:47:02 +0700

    แก้ไขฟังก์ชัน header ให้มีความสวยงามและ Alert เมื่อ login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,63 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
-import './Header.css';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+  const navigate = useNavigate();
+  const Swal = require('sweetalert2')
 
   function loginClick() {
-    const confirmBox = window.confirm('คุณต้องการที่จะเข้าสู่ระบบหรือไม่? ฟังชั่นนี้มีไว้สำหรับผู้ที่เข้าร่วมแข่งขันเท่านั้น');
-    if (confirmBox === true) {
-      window.location.href = "/announceR1";
-    }
+      localStorage.removeItem('user')
+      Swal.fire({
+          icon: "warning",
+          title: 'คุณต้องการเข้าสู่ระบบหรือไม่?',
+          text: 'สำหรับผู้เข้าแข่งขัน NSC เท่านั้น',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'ตกลง',
+          cancelButtonText: 'ยกเลิก',
+          confirmButtonWidth: 2000
+      }).then((result) => {
+          if (result.isConfirmed) {;
+              navigate('/login');
+          }
+          else if (
+              result.dismiss === Swal.DismissReason.cancel) {
+                  navigate('/');
+            }
+        })
+  
   }
 
   return (
-    <Navbar expand="lg" bg="navbar navbar-dark bg-dark" style={{paddingBottom:'22.5px'}}>
+    <Navbar expand="lg" bg="light" style={{paddingLeft:'22.5px', paddingRight: '22.5px'}}>
       <Container>
-        <Navbar.Brand href="/">
-        <div style={{width:'50px',height:'40px'}}> 
-          <img src='../images/LOGO-NSC.png' style={{width:'100%',borderRadius:'100%'}}/>
-        </div>   
+        <Navbar.Brand href="/main"><img className='NavLogo' src='https://www.nectec.or.th/sectionImage/1339' alt='logo' />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto" style={{columnGap:'20px'}}>
-            <Nav.Link className='nav_link' href="/" style={{color:'white'}}>หน้าหลัก</Nav.Link>
-            <NavDropdown title="การแข่งขัน" id="basic-nav-dropdown" variant="seconddary">
-              <NavDropdown.Item href="/topic">หัวข้อการแข่งขัน</NavDropdown.Item>
+            <Nav.Link className='nav_link' href="/main">หน้าหลัก</Nav.Link>
+            <NavDropdown className="bar1" title="การแข่งขัน" id="basic-nav-dropdown" variant="seconddary">
+              <NavDropdown.Item className="bar1_1" href="/topic">หัวข้อการแข่งขัน</NavDropdown.Item>
               <hr />
-              <NavDropdown.Item href="">กำหนดการ</NavDropdown.Item>
+              <NavDropdown.Item className="bar1_2" href="">กำหนดการ</NavDropdown.Item>
               <hr />
-              <NavDropdown.Item href="">คุณสมบัติ</NavDropdown.Item>
+              <NavDropdown.Item className="bar1_3"href="">คุณสมบัติ</NavDropdown.Item>
               <hr />
-              <NavDropdown.Item href="placecontac">สถานที่ติดต่อ</NavDropdown.Item>
+              <NavDropdown.Item className="bar1_4" href="placecontac">สถานที่ติดต่อ</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="ผลการแข่งขัน" id="basic-nav-dropdown" >
-              <NavDropdown.Item href="/announceR1">รอบที่ 1</NavDropdown.Item>
+            <NavDropdown className="bar2" title="ผลการแข่งขัน" id="basic-nav-dropdown" >
+              <NavDropdown.Item className="bar2_1" href="/announceR1">รอบที่ 1</NavDropdown.Item>
               <hr />
-              <NavDropdown.Item href="/announceR2">รอบที่ 2</NavDropdown.Item>
+              <NavDropdown.Item className="bar2_2" href="/announceR2">รอบที่ 2</NavDropdown.Item>
             </NavDropdown>
-            <Button className="confirm-dialog-button" variant="outline-primary"  onClick={loginClick}>เข้าสู่ระบบ</Button>{' '}
+            <Button className="confirm-dialog-button" /*variant="outline-primary"*/  onClick={loginClick}>เข้าสู่ระบบ</Button>{' '}
           </Nav>
         </Navbar.Collapse>
       </Container>
