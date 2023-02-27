e1cc2da2ecf0e201f4eadb84e427750e60ae9315
psu6510110004 <dragonslayertitle@gmail.com>
2023-02-27 17:34:43 +0700

    แก้ไข path

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+//import logo from './logo.svg';
+//import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import Announce from './components/Announce';
+//import { Container } from "reactstrap";
+import Home from './components/Home';
+import Login from './components/login';
+import Logout from './components/logout';
 import Main from './components/Main';
-
+import Registration from './components/regis';
 
 function App() {
   return (
-    <>
-      {/* <hr style={{marginTop:'5px',marginBottom:'0',marginLeft:'auto',marginRight:'auto',border:'.5px solid #333',width:'90%'}}/> */}
-      <Header />
-      {/* <hr style={{margin:'0 auto',border:'.5px solid #333',width:'90%'}}/> */}
-      <Main />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <Footer />
-    </>
+  //<Container>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/logout' element={<Logout />} />
+      <Route path='/announce' element={<Announce />} />
+      <Route path='/regis' element={<Registration />} />
+      <Route path='/main' element={<Main />} />
+    </Routes>
+  </BrowserRouter>
+  //</Container>
   );
-}
+};
 
 export default App;
