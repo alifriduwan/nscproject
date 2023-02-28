1a8331ba87f1550f6b1d2a8210f6886d729f1b28
psu6510110004 <dragonslayertitle@gmail.com>
2023-02-28 17:04:17 +0700

    แก้ไขการ merge

--- client/src/App.js
+++ client/src/App.js
@@ -15,11 +15,8 @@ function App() {
     <Routes>
       <Route path='/' element={<Main />} />
       <Route path='/login' element={<Login />} />
-<<<<<<< HEAD
       <Route path='/mainlog' element={<MainLog />} />
-=======
       <Route path='/activity' element={<Activity />} />
->>>>>>> 64a8b2577f38e3eb21d7d725bd724cac512da90e
     </Routes>
   </BrowserRouter>
   //</Container>
