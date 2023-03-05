e15362393e35775efa6b4362ad6372aa50f8b196
samutthai <psu6510110480@email.psu.ac.th>
2023-03-05 16:16:26 +0700

    Update Database and config page

--- Server/src/api/result-round1/services/result-round1.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * result-round1 service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::result-round1.result-round1');
