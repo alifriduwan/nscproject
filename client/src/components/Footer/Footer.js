a6ff1fe3af30ed0057121b420a9cb9a23d4c4e76
อาลีฟริดฎูวาน อุมา <alifriduwanhellowold17@gmail.com>
2023-03-06 22:40:53 +0700

    ตกแต่ง main และทำ responsive

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div>
-      <div id="page-content" style={{ minHeight: '100vh', position: 'relative' }}>
+      <div id="page-content" style={{ position: 'relative' }}>
         {/* your page content */}
       </div>
       <footer style={{ textAlign: 'center', backgroundColor:'rgb(240, 240, 240)', padding:20, position: 'absolute', bottom: 0, width: '100%' }}>
--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Main.css'
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 
@@ -8,14 +8,15 @@ import Footer from '../../components/Footer/Footer';
 
 function Main() {
   return (
-     <div>
+     <>
           <Header />
+          <div style={{maxHeight:'100px'}} className='space-top'></div>
           <div class="container-md">
                <div class="container-content" style={{marginTop:'25px'}}>
                     <div style={{marginLeft:'30px'}}>
-                         <h1 className='display-6' style={{marginBottom:'0'}} >ศูนย์ประสานงานการแข่งขัน NSC Southern Thailand</h1>
+                         <h1 className='display-6' style={{marginBottom:'0',paddingTop:'25px'}} >ศูนย์ประสานงานการแข่งขัน NSC ระดับภาคใต้</h1>
                          {/* <h1 style={{display:'inline-block',marginBottom:'0'}}>โปรแกรมคอมพิวเตอร์แห่งประเทศไทย</h1> */}
-                         <p style={{paddingTop:'4.5px'}}>The 24th National Software Contest:NSC 2022</p>
+                         <p style={{paddingTop:'4.5px'}} className='p-head'>The 24th National Software Contest:NSC 2022</p>
                     </div>
                </div>
           </div>
@@ -24,13 +25,13 @@ function Main() {
                <div style={{marginTop:'25px'}}>
                     <div style={{marginLeft:'30px'}}>
                          <h5 style={{marginBottom:'0'}}>ขอเชิญชวนนักเรียน นิสิตและนักศึกษา</h5>
-                         <p style={{paddingTop:'17px'}}>ที่มีความสามารถในการพัฒนาซอฟต์แวร์ส่งผลงานเข้าร่วมการแข่งขันพัฒนาโปรแกรมคอมพิวเตอร์แห่งประเทศไทย ชิงถ้วยพระราชทานสมเด็จพระกนิษฐาธิราชเจ้า กรมสมเด็จพระเทพรัตนราชสุดาฯ สยามบรมราชกุมารี พร้อมรางวัลและเกียรติบัตร</p>     
-                         <Button variant="outline-primary">ข่าวสารและประชาสัมพันธ์</Button>{' '}
+                         <p style={{paddingTop:'5px'}}>ที่มีความสามารถในการพัฒนาซอฟต์แวร์ส่งผลงานเข้าร่วมการแข่งขันพัฒนาโปรแกรมคอมพิวเตอร์แห่งประเทศไทย ชิงถ้วยพระราชทานสมเด็จพระกนิษฐาธิราชเจ้า กรมสมเด็จพระเทพรัตนราชสุดาฯ สยามบรมราชกุมารี พร้อมรางวัลและเกียรติบัตร</p>     
+                         <a href='#' className='btn-ข่าวสารและประชาสัมพันธ์'>ข่าวสารและประชาสัมพันธ์</a>
                     </div>
                </div>
           </div>
           <Footer />
-     </div> 
+     </> 
   )
 }
 
