cd8806c42854947def3efc54933d1d66fb71bcac
Phitchaphat Saelee <saeleebright@gmail.com>
2023-03-09 04:20:40 +0700

    แก้ไขหน้าupdate

--- client/src/pages/Admin/UserUpdate.js
+++ client/src/pages/Admin/UserUpdate.js
@@ -1,164 +1,210 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
+import React, { useEffect, useState} from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import { Grid,TextField,Typography } from '@mui/material';
+import Header from '../../components/Header/Header';
+import { Button } from 'reactstrap';
+import { userData } from '../../helper';
+import { useParams } from 'react-router-dom';
 
-export default function UserUpdate() {  
-  const { id } = useParams();
-  useEffect(() => {
-    fetch("http://localhost:1337/api/usertables"+id)
-      .then(res => res.json())
-      .then(
-        (result) => {
 
-            setCode(result.attributes.Code)
-            setProject(result.attributes.Project)
-            setSchool(result.attributes.School)
-            setAdvisor(result.attributes.Advisor)
-            setStudent1(result.attributes.Student1)
-            setStudent2(result.attributes.Student2)
-            setStudent3(result.attributes.Student3)
-            setRound1(result.attributes.Round1)
-            setScholar(result.attributes.Scholar)
-            setFinal(result.attributes.Final)
-         
+
+export default function UserUpdate() {
+    const {id} = useParams();
+    useEffect(() => {
+    const user = userData();
+        if (user && user.jwt) {
+            var myHeaders = new Headers();
+                myHeaders.append("Authorization", "Bearer " + user.jwt);
+
+            var requestOptions = {
+                method: 'GET',
+                headers: myHeaders,
+                redirect: 'follow'
+      };
+
+      fetch("http://localhost:1337/api/users/me?populate=*"+id, requestOptions)
+      .then(response => response.json())
+      .then(result => {
+       
+        if (result && result.role && result.role.name === "Authenticated") {
+          setIsAuthenticated(true);
         }
-      )
-  }, [id])
+      })
+      .catch(error => console.log('error', error));
+    }
+        fetch("http://localhost:1337/api/usertables")
+        .then(res => res.json())
+        .then(
+            (result) => {
+            setItems(result.data);
+            },
 
+        )
+  }, [id])
   const handleSubmit = event => {
     event.preventDefault();
+    const user = userData();
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + user.jwt);
+    myHeaders.append("Content-Type", "application/json");
+
     var raw = JSON.stringify(
 
-        {
-          "data": {
-            "Code": Code,
-            "Project": Project,
-            "Class": Class,
-            "Level": Level,
-            "Advisor": Advisor,
-            "School": School,
-            "Student1": Student1,
-            "Student2": Student2,
-            "Student3": Student3,
-            "Scholar": Scholar,
-            "Final": Final,
-            "Round1": Round1,
-            "relation": 2,
-            "relations": [2, 4],
-            "link": {
-              "id": 1,
-              "type": "abc"
-            }
+      {
+        "data": {
+          "Code": Code,
+          "Project": Project,
+          "Class": Class,
+          "Level": Level,
+          "Advisor": Advisor,
+          "School": School,
+          "Student1": Student1,
+          "Student2": Student2,
+          "Student3": Student3,
+          "Scholar": Scholar,
+          "Final": Final,
+          "Round1": Round1,
+          "relation": 2,
+          "relations": [2, 4],
+          "link": {
+            "id": 1,
+            "type": "abc"
           }
         }
-    );
-    fetch('http://localhost:1337/api/usertables', {
-      method: 'PUT',
-      headers: {
-        Accept: 'application/form-data',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    })
-    .then(res => res.json())
+      }
+
+  );
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+};
+
+  fetch("http://localhost:1337/api/usertables", requestOptions)
+    .then(response => response.json())
     .then(
       (result) => {
-        alert(result['message'])
-        if (result['status'] === 'ok') {
-          window.location.href = '/';
-        }
+      
+      if (Code && Project && Class && Level && School && Advisor && Student1 && Student2 && Student3 && Round1 && Scholar && Final  !== "" ){
+        window.location.href = 'Admin'
       }
-    )
+    })
+    .catch(error => console.log('error', error));
   }
+  
+  const [Code,setCode] = useState('');
+  const [Project,setProject] = useState('');
+  const [Class,setClass] = useState('');
+  const [Level,setLevel] = useState('');
+  const [School,setSchool] = useState('');
+  const [Advisor,setAdvisor] = useState('');
+  const [Student1,setStudent1] = useState('');
+  const [Student2,setStudent2] = useState('');
+  const [Student3,setStudent3] = useState('');
+  const [Round1,setRound1] = useState('');
+  const [Scholar,setScholar] = useState('');
+  const [Final,setFinal] = useState('');
+  const [items,setItems] = useState([]);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const [fname, setFname] = useState('');
-  const [lname, setLname] = useState('');
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [avatar, setAvatar] = useState('');
+ 
+
   return (
-    <Container sx={{ p:2 }} maxWidth="sm">    
-      <div>
-        <Typography component="h1" variant="h5">
-          User
-        </Typography>
-        <form onSubmit={handleSubmit}>
-          <Grid container sx={{ pt:2 }} spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                value={fname}
-                onChange={(e) => setFname(e.target.value)}
-                autoFocus
-              />
+    <div>
+    <Header/>
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="sm" sx={{p:2}}>
+          <Typography variant='h6' gutterBootom component='div'>
+            Update user
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField id="Code" label="รหัสโครงการ" variant="outlined" 
+                fullWidth required
+                onChange={(e) => setCode(e.target.value)} />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField id="Project" label="โครงการ" variant="outlined" 
+                fullWidth required 
+                onChange={(e) => setProject(e.target.value)} />
+
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField id="Class" label="หมวดโครงการ" variant="outlined" 
+                fullWidth required 
+                onChange={(e) => setClass(e.target.value)} />
+
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField id="Level" label="ระดับ" variant="outlined" 
+                fullWidth required 
+                onChange={(e) => setLevel(e.target.value)} />
+
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField id="School" label="สถาบัน" variant="outlined" 
+                fullWidth required 
+                onChange={(e) => setSchool(e.target.value)} />
+
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField id="Advisor" label="อาจาร์ยที่ปรึกษา" variant="outlined" 
+                fullWidth required 
+                onChange={(e) => setAdvisor(e.target.value)} />
+
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField id="Student1" label="ผู้พัฒนาคนที่ 1" variant="outlined" 
+                fullWidth required 
+                onChange={(e) => setStudent1(e.target.value)} />
+
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField id="Student2" label="ผู้พัฒนาคนที่ 2" variant="outlined" 
+                fullWidth required 
+                onChange={(e) => setStudent2(e.target.value)} />
+
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField id="Student3" label="ผู้พัฒนาคนที่ 3" variant="outlined" 
+                fullWidth required 
+                onChange={(e) => setStudent3(e.target.value)} />
+
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField id="Scholar" label="เข้ารอบได้รับเกียรติบัตร" variant="outlined" 
+                fullWidth required 
+                onChange={(e) => setScholar(e.target.value)} />
+
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField id="Final" label="ผ่านเข้าชิง" variant="outlined" 
+                fullWidth required 
+                onChange={(e) => setFinal(e.target.value)} />
+
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField id="Round1" label="ภาค" variant="outlined" 
+                fullWidth required 
+                onChange={(e) => setRound1(e.target.value)} />
+
+              </Grid>
+              <Grid item xs={12} >
+                <Button type="submit" 
+                variant="contained" 
+                fullWidth >Create</Button>
+
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                value={lname}
-                onChange={(e) => setLname(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="username"
-                label="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="avatar"
-                label="Avatar"
-                value={avatar}
-                onChange={(e) => setAvatar(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-              >
-                Submit
-            </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container>
+          </form>
+        </Container>
+      </React.Fragment>
+    </div>  
   );
 }
