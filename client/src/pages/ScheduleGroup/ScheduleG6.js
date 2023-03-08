08d6f268a511b26949d558d49fc6889f40b7afcc
psu6510110004 <dragonslayertitle@gmail.com>
2023-03-09 01:40:56 +0700

    ตกแต่งเพิ่มเติม

--- client/src/pages/ScheduleGroup/ScheduleG6.js
+++ client/src/pages/ScheduleGroup/ScheduleG6.js
@@ -2,9 +2,10 @@ import React from 'react'
 import Table from 'react-bootstrap/Table';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
-import ButtonForSchedule from '../../components/ButtonForSchedule';
+import ButtonForSchedule from '../../components/Button/ButtonForSchedule';
 import { userData } from '../../helper';
 import WarnLog from '../../components/Card/WarnLog';
+import './TimeG.css'
 
 function ScheduleG6() {     
      const data = [
@@ -25,64 +26,66 @@ function ScheduleG6() {
   }
   
   return (
-     <>
-          <Header />
-
-          <div style={{maxWidth:'960px',margin:"15px auto"}}>
-          <div style={{textAlign:'center',backgroundColor:'#3C84AB',overflow:'hidden',borderRadius:'9px',color:'white'}}>
-            <h6>ตารางการแข่งขัน</h6>
-          </div>    
-          <div style={{textAlign:'center',backgroundColor:'#1699E1', overflow:'hidden',borderRadius:'9px',color:'white'}}>
-            <h6>
-              รอบ 2 พิจารณาผลงาน (รูปแบบออนไลน์)
-            </h6>
-          </div>
-          <div style={{textAlign:'center',backgroundColor:'#C1AEFC', overflow:'hidden',borderRadius:'9px',color:'white'}}>
-            <h6>
-              วันจันทร์ที่ 9 กุมภาพันธ์ 2565
-            </h6>
-          </div>
-          <div style={{textAlign:'center',backgroundColor:'#C1AEFC',overflow:'hidden',borderRadius:'9px',color:'white'}}>
-            <h6>
-              การนำเสนอผลงาน แต่ละโครงการใช้เวลา 20 นาที (นำเสนอ 15 นาที + ตอบคำถามกรรมการ 5 นาที)
-            </h6>
-          </div> 
-          <Table striped bordered hover variant="dark" className='table-rd'>
-      <thead>
-        <tr>
-          <th style={thStyle}>กลุ่ม</th>
-          <th style={thStyle}>หมวด</th>
-          <th style={thStyle}>เวลานำเสนอ</th>
-          <th style={thStyle}>รหัสโครงการ</th>
-          <th style={thStyle}>ชื่อโครงการ</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item) => (
-          <tr key={item.id}>
-            <td style={thStyle}>6</td>
-            <td style={thStyle}>{item.group}</td>
-            <td style={thStyle}>{item.timePresent}</td>
-            <td style={thStyle}>{item.IdProject}</td>
-            <td>{item.nameProject}</td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
-    <div style={{color:'black',paddingLeft:'10px'}}>
-      <h6>ลิงก์การแข่งขัน : https://meet.google.com/wma-xked-kor</h6>
-    </div>
-    <div style={{color:'red',paddingLeft:'10px'}}>
-      <h6>โปรดรักษาวลามาให้ตรงตามเวลาที่กำหนด * </h6>
-    </div>
-    </div>
-    <br />
-    <ButtonForSchedule />
-    <div style={{height:'100px'}}></div>
-    <Footer />
-    {/* </div> */}
-     </>
-  )
+    <>
+     <Header />
+       <div className='container'>
+         <div className="card mt-5">
+           <div className="card-body">
+             <div style={{maxWidth:'960px',margin:"15px auto"}}>
+                 <div className='HeadTable' style={{backgroundColor:'#3C84AB',overflow:'hidden',borderRadius:'9px',color:'white'}}>
+                   <h6>ตารางการแข่งขัน</h6>
+                 </div>    
+                 <div className='SecTable' style={{textAlign:'center',backgroundColor:'#1699E1', overflow:'hidden',borderRadius:'9px',color:'white'}}>
+                   <h6>
+                     รอบ 2 พิจารณาผลงาน (รูปแบบออนไลน์)
+                   </h6>
+                 </div>
+                 <div className='ThdTable' style={{textAlign:'center',backgroundColor:'#C1AEFC', overflow:'hidden',borderRadius:'9px',color:'white'}}>
+                   <h6>
+                     วันจันทร์ที่ 9 กุมภาพันธ์ 2565
+                   </h6>
+                 </div>
+                 <div style={{textAlign:'center',backgroundColor:'#C1AEFC',overflow:'hidden',borderRadius:'9px',color:'white'}}>
+                   <h6>
+                     การนำเสนอผลงาน แต่ละโครงการใช้เวลา 20 นาที (นำเสนอ 15 นาที + ตอบคำถามกรรมการ 5 นาที)
+                   </h6>
+                 </div>
+                 <Table striped bordered hover variant="dark" responsive="md" style={{width:'100%',borderRadius:'9px'}}> 
+                   <thead style={{borderRadius:'9px'}}>
+                     <tr className='TableHeader'>
+                       <th style={thStyle}>กลุ่ม</th>
+                       <th style={thStyle}>หมวด</th>
+                       <th style={thStyle}>เวลานำเสนอ</th>
+                       <th style={thStyle}>รหัสโครงการ</th>
+                       <th style={thStyle}>ชื่อโครงการ</th>
+                     </tr>
+                   </thead>
+                   <tbody>
+                     {data.map((item) => (
+                       <tr className='TableSec' key={item.id} style={{overflow:'hidden'}}>
+                         <td style={thStyle}>1</td>
+                         <td style={thStyle}>{item.group}</td>
+                         <td style={thStyle}>{item.timePresent}</td>
+                         <td style={thStyle}>{item.IdProject}</td>
+                         <td style={thStyle}>{item.nameProject}</td>
+                       </tr>
+                     ))}
+                   </tbody>
+                 </Table>
+             <div className='Meeting' style={{color:'black',paddingLeft:'10px'}}>
+               <h6>ลิงก์การแข่งขัน : https://meet.google.com/wma-xked-kor</h6>
+             </div>
+             <div className='Warnning' style={{color:'red',paddingLeft:'10px'}}>
+               <h6>โปรดรักษาวลามาให้ตรงตามเวลาที่กำหนด * </h6>
+             </div>
+           </div>
+           <br />
+           <ButtonForSchedule />
+         </div>
+       </div>
+     </div>
+    </>
+ )
 }
 
 export default ScheduleG6
--- client/src/pages/Schedule_MTG/Schedule_MTG3.js
+++ client/src/pages/Schedule_MTG/Schedule_MTG3.js
@@ -5,6 +5,7 @@ import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import WarnLog from '../../components/Card/WarnLog';
 import { userData } from '../../helper';
+import './MTG.css'
 
 function Scheddule_MTG3() {     
      const data = [
@@ -25,48 +26,51 @@ function Scheddule_MTG3() {
   }
 
   return (
-     <>
-          <Header />
-      <div style={{maxWidth:'960px',margin:"15px auto"}}>
-          <div style={{textAlign:'center',backgroundColor:'#3C84AB',overflow:'hidden',borderRadius:'9px',color:'white'}}>
-            <h6>ตารางเข้าร่วมประชุมออนไลน์</h6>
-          </div>    
-          <div style={{textAlign:'center',backgroundColor:'#C1AEFC', overflow:'hidden',borderRadius:'9px',color:'white'}}>
-            <h6>
-              วันจันทร์ที่ 9 กุมภาพันธ์ 2565
-            </h6>
-          </div>
-          <Table striped bordered hover variant="dark" responsive="md" style={{width:'100%',borderRadius:'9px'}}> 
-      <thead style={{borderRadius:'9px'}}>
-        <tr>
-          <th style={thStyle}>กลุ่ม</th>
-          <th style={thStyle}>หมวด</th>
-          <th style={thStyle}>ชื่อโครงการ</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item) => (
-          <tr key={item.id} style={{overflow:'hidden'}}>
-            <td style={thStyle}>1</td>
-            <td style={thStyle}>{item.group}</td>
-            <td>{item.nameProject}</td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
-    <div style={{color:'black',paddingLeft:'10px'}}>
-      <h6>ลิงก์การประชุม : https://meet.google.com/wma-xked-kor</h6>
-    </div>
-    <div style={{color:'red',paddingLeft:'10px'}}>
-      <h6>เวลาการประชุม : ตั้งแต่ 15:00 น. เป็นต้นไป </h6>
-    </div>
-    </div>
-    <div style={{height:'120px'}}></div>
-    <ButtonForMeet />
-    <div style={{height:'110px'}}></div>
-    <Footer />
-     </>
-  )
+    <>
+     <Header />
+       <div className='container'>
+         <div className="card mt-5">
+           <div className="card-body">
+             <div style={{maxWidth:'960px',margin:"15px auto"}}>
+                 <div className='HeadMT' style={{textAlign:'center',backgroundColor:'#3C84AB',overflow:'hidden',borderRadius:'9px',color:'white'}}>
+                   <h6>ตารางเข้าร่วมประชุมออนไลน์</h6>
+                 </div>    
+                 <div className='SecMT' style={{textAlign:'center',backgroundColor:'#C1AEFC', overflow:'hidden',borderRadius:'9px',color:'white'}}>
+                   <h6>
+                     วันจันทร์ที่ 9 กุมภาพันธ์ 2565
+                   </h6>
+                 </div>
+                 <Table striped bordered hover variant="dark" responsive="md" style={{width:'100%',borderRadius:'9px'}}> 
+                    <thead style={{borderRadius:'9px'}}>
+                       <tr className='HeadTab'>
+                         <th style={thStyle}>กลุ่ม</th>
+                         <th style={thStyle}>หมวด</th>
+                         <th style={thStyle}>ชื่อโครงการ</th>
+                       </tr>
+                     </thead>
+                     <tbody>
+                       {data.map((item) => (
+                         <tr className='SecTab' key={item.id} style={{overflow:'hidden'}}>
+                           <td style={thStyle}>1</td>
+                           <td style={thStyle}>{item.group}</td>
+                           <td style={thStyle}>{item.nameProject}</td>
+                         </tr>
+                       ))}
+                     </tbody>
+                   </Table>
+                     <div className='meetlink' style={{color:'black',paddingLeft:'10px'}}>
+                       <h6>ลิงก์การประชุม : https://meet.google.com/wma-xked-kor</h6>
+                     </div>
+                     <div className='meettime' style={{color:'red',paddingLeft:'10px'}}>
+                       <h6>เวลาการประชุม : ตั้งแต่ 15:00 น. เป็นต้นไป </h6>
+                     </div>
+                 </div>
+               <ButtonForMeet />
+         </div>
+       </div>
+     </div>
+    </>
+ )
 }
 
 export default Scheddule_MTG3
