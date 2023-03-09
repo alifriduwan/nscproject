7346cc7d4ca3520f6f3114d88ad04b259a1f462e
psu6510110004 <dragonslayertitle@gmail.com>
2023-03-09 15:26:05 +0700

    แก้ไขการแสดงผลผิดพลาดเล็กน้อย

--- client/src/pages/PlaceContact/PlaceContact.js
+++ client/src/pages/PlaceContact/PlaceContact.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table';
 import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer'
-<<<<<<< HEAD
-=======
 import './PlaceContact.css'
->>>>>>> 38b69476a6510b032d409b510d03da4cd0e9e201
 
 function PlaceContact() {
   return (
@@ -26,13 +22,10 @@ function PlaceContact() {
               <div>
                 <p>คณะวิศวกรรมศาสตร์ มหาวิทยาลัยสงขลานครินทร์</p>
                 <p>โทร: 074-287-076</p>
-<<<<<<< HEAD
                 <p>อีเมลล์: wimon@coe.psu.ac.th</p>
                 <p>เฟซบุ๊ก: NSC Thailand Southern Region-(PSU)</p>
-=======
                 <a href='mailto: wimon@coe.psu.ac.th' target={'_blank'} className='link-email'><p>อีเมลล์: wimon@coe.psu.ac.th</p></a>
                 <a href='https://web.facebook.com/nsc.th' target={'_blank'} className='link-facebook'><p>เฟซบุ๊ก: NSC Thailand Southern Region-(PSU)</p></a>
->>>>>>> 38b69476a6510b032d409b510d03da4cd0e9e201
               </div>
             </tr>
           </tbody>
